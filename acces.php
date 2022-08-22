19bd3753e4c35015809491311ec459a43c340ca6
lamiaekhad <102177275+lamiaekhad@users.noreply.github.com>
2022-08-22 14:22:42 -0400

    modification

--- acces.php
+++ acces.php
@@ -15,9 +15,9 @@
             <div id="mySidenav" class="sidenav">
                 <a id="closeBtn" href="#" class="close">×</a>
                     <ul>
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="produit.php">Produits</a></li>
-                        <li><a href="contact.php">Contact</a></li>
+                        <li><a href="index.php">Home</a></li><br>
+                        <li><a href="produit.php">Produits</a></li><br>
+                        <li><a href="contact.php">Contact</a></li><br>
                         <li><a href="acces.php">Accès</a></li>
                     </ul>
             </div>
@@ -68,7 +68,7 @@
 <!--acces + panier version desktop-->
             <div class="wrapi1">
                <div class="search">
-                  <input type="text" class="searchTerm" placeholder="   Recherche...">
+                  <input type="search" class="searchTerm" placeholder="   Recherche...">
                   <button type="submit" class="searchButton">
                     <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">    <path d="M 9 2 C 5.1458514 2 2 5.1458514 2 9 C 2 12.854149 5.1458514 16 9 16 C 10.747998 16 12.345009 15.348024 13.574219 14.28125 L 14 14.707031 L 14 16 L 20 22 L 22 20 L 16 14 L 14.707031 14 L 14.28125 13.574219 C 15.348024 12.345009 16 10.747998 16 9 C 16 5.1458514 12.854149 2 9 2 z M 9 4 C 11.773268 4 14 6.2267316 14 9 C 14 11.773268 11.773268 14 9 14 C 6.2267316 14 4 11.773268 4 9 C 4 6.2267316 6.2267316 4 9 4 z"/></svg>
                  </button>
@@ -76,7 +76,6 @@
             </div>
 
             <div class="acces_panier">
-                
                 
                 <a class="flex_acces_panier"  href="acces.php">
                     <div class="text_acces_panier">Accès</div>
@@ -100,10 +99,10 @@
         <div class="trait_dessus"></div>
     </header>
    
-<section>
+<section class="">
     <div class="wrapi2">
                <div class="search">
-                  <input type="text" class="searchTerm" placeholder="   Recherche...">
+                  <input type="search" class="searchTerm" placeholder="   Recherche...">
                   <button type="submit" class="searchButton">
                     <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">    <path d="M 9 2 C 5.1458514 2 2 5.1458514 2 9 C 2 12.854149 5.1458514 16 9 16 C 10.747998 16 12.345009 15.348024 13.574219 14.28125 L 14 14.707031 L 14 16 L 20 22 L 22 20 L 16 14 L 14.707031 14 L 14.28125 13.574219 C 15.348024 12.345009 16 10.747998 16 9 C 16 5.1458514 12.854149 2 9 2 z M 9 4 C 11.773268 4 14 6.2267316 14 9 C 14 11.773268 11.773268 14 9 14 C 6.2267316 14 4 11.773268 4 9 C 4 6.2267316 6.2267316 4 9 4 z"/></svg>
                  </button>
@@ -112,14 +111,21 @@
     <h1 class="titre_acces">
         Accès
     </h1>
-    <p class="text_please_fill"> Hello, enter your username and password:</p>
-    <form  action="acces.php">
-        <input class="input_text" type="text" id="fname" name="User" placeholder="User">
-        <input class="input_text" type="text" id="Sname" name="Password" placeholder="Password">
-    </form>
-    <div class="button_start_with1">
-            <button class="start_with1">  Ajouter au panier</button>
+    <p class="text_login"> Hello, enter your username and password:</p>
+    <div class="login_container">
+        <div class="input_Acces">
+        <form  action="acces.php">
+            <input class="input_text" type="text" id="fname" name="User" placeholder="User">
+            <input class="input_text" type="password" id="Sname" name="Password" placeholder="Password">
+        </form>
         </div>
+
+        <div class="button_start_with1">
+            <a href="compte_admin.php"><button class="start_with1">  Enter </button></a>
+        </div>
+        <p class="text_forget">Forgot your password?</p>
+    </div>
+   
 </section>
 
     <footer>
--- panier.php
+++ panier.php
@@ -16,9 +16,9 @@
             <div id="mySidenav" class="sidenav">
                 <a id="closeBtn" href="#" class="close">×</a>
                     <ul>
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="produit.php">Produits</a></li>
-                        <li><a href="contact.php">Contact</a></li>
+                        <li><a href="index.php">Home</a></li><br>
+                        <li><a href="produit.php">Produits</a></li><br>
+                        <li><a href="contact.php">Contact</a></li><br>
                         <li><a href="acces.php">Acces</a></li>
                     </ul>
             </div>
@@ -69,7 +69,7 @@
 <!--acces + panier version desktop-->
             <div class="wrapi1">
                <div class="search">
-                  <input type="text" class="searchTerm" placeholder="   Recherche...">
+                  <input type="search" class="searchTerm" placeholder="   Recherche...">
                   <button type="submit" class="searchButton">
                     <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">    <path d="M 9 2 C 5.1458514 2 2 5.1458514 2 9 C 2 12.854149 5.1458514 16 9 16 C 10.747998 16 12.345009 15.348024 13.574219 14.28125 L 14 14.707031 L 14 16 L 20 22 L 22 20 L 16 14 L 14.707031 14 L 14.28125 13.574219 C 15.348024 12.345009 16 10.747998 16 9 C 16 5.1458514 12.854149 2 9 2 z M 9 4 C 11.773268 4 14 6.2267316 14 9 C 14 11.773268 11.773268 14 9 14 C 6.2267316 14 4 11.773268 4 9 C 4 6.2267316 6.2267316 4 9 4 z"/></svg>
                  </button>
@@ -106,7 +106,7 @@
     <section id="prduit1" class="section_p1">
         <div class="wrapi2">
            <div class="search">
-              <input type="text" class="searchTerm" placeholder="   Recherche...">
+              <input type="search" class="searchTerm" placeholder="   Recherche...">
               <button type="submit" class="searchButton">
                 <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">    <path d="M 9 2 C 5.1458514 2 2 5.1458514 2 9 C 2 12.854149 5.1458514 16 9 16 C 10.747998 16 12.345009 15.348024 13.574219 14.28125 L 14 14.707031 L 14 16 L 20 22 L 22 20 L 16 14 L 14.707031 14 L 14.28125 13.574219 C 15.348024 12.345009 16 10.747998 16 9 C 16 5.1458514 12.854149 2 9 2 z M 9 4 C 11.773268 4 14 6.2267316 14 9 C 14 11.773268 11.773268 14 9 14 C 6.2267316 14 4 11.773268 4 9 C 4 6.2267316 6.2267316 4 9 4 z"/></svg>
              </button>
