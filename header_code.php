abe2b50e6596b8fafcb88d2a044cb98f4936b6d1
lamiaekhad <102177275+lamiaekhad@users.noreply.github.com>
2022-10-06 14:15:39 -0400

    modifications

--- header_code.php
+++ header_code.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script scr="main.js" defer> </script>
+<script scr="script/main.js" defer> </script>
     <title>projet_final</title>
 </head>
 <body>
@@ -16,7 +16,7 @@
            
 <!--menu + logo version mobile-->
            <div id="mySidenav" class="sidenav">
-               <a id="closeBtn" href="#" class="close">×</a>
+               <a id="closeBtn" href="#" class="close">x</a>
                    <ul>
                        <li><a href="Home.php">Home</a></li><br>
                        <li><a href="index.php">Produits</a></li><br>
@@ -83,9 +83,6 @@
                 </form>
            </div>
           
-
-       
-       
            <div class="acces_panier">
                
                <a class="flex_acces_panier"  href="acces.php">
